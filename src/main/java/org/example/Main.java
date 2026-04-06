04d118d00d6314b11922acce215fed7172683599
christopher Lanier <crlanier22@gmail.com>
2026-04-05 22:53:54 -0400

    added move set to Maki and running total of Sakuna's health

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,31 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sakuna just entered Tokyo Colony!");
         System.out.println("Maki, Yuta and Yuji are our only hope to stop him and save the world!");
+        System.out.println("");
         Scanner sc = new Scanner(System.in);
         System.out.println("Choose your player, 1.Maki 2.Yuta 3.Yuji");
         int userPlayer = sc.nextInt();
+        int sakunaHealth = 100;
         switch (userPlayer){
             case 1:
                 System.out.println("Great chice! You've chosen Maki of the Zenin Clan");
                 System.out.println("known for her having heavenly restriction and weilding\n"+"the split-soul katana, Maki will put up a great fight!");
+                System.out.println("");
+                System.out.println("Choose your move but hurry, Sakuna is approaching!");
+                System.out.println("1.Split-Soul katana slice 2.Dragon bone vertical Slash");
+                int playerMove = sc.nextInt();
+                if(playerMove == 1){
+                    System.out.println("Maki used Split-Soul katana slice.\n This is highly effective Sakuna just lost 30% of his health!");
+                    sakunaHealth-=30;
+                    System.out.println("He's at "+sakunaHealth+" health now, keep pressing forward");
+                } else if (playerMove == 2) {
+                    System.out.println("Maki used Dragon bone vertical Slash");
+                }
+
                 break;
             case 2:
-                System.out.println("Great chice! You've chosen Yuta Okkotsu");
-                System.out.println("Being 2nd to Gojo Yuta and with Rika by his side, Sakuna won't stand a chance!");
+                System.out.println("Great choice! You've chosen Yuta Okkotsu");
+                System.out.println("Being 2nd to Gojo, Yuta with Rika by his side, Sakuna won't stand a chance!");
                 break;
             case 3:
                 System.out.println("Great chice! You've chosen Yuji Itadori");
